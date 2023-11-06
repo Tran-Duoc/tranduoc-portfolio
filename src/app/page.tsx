612b18511a50d32a0f8d76cf8a9553f1ea0f58fc
Tran Duoc <duoctran2527@gmail.com>
2023-11-06 16:03:52 +0700

    add intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className=' min-h-screen'>
       <Header />
-      {/* <Intro /> */}
+      <Intro />
       <About />
       <>
         <SectionTouch />
